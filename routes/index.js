d6a56f1d40cc44bf13052ecef76d256cce3808d2
lixin65535@126.com <lixin65535@126.com>
2018-02-07 17:13:10 +0800

    #

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var dboper = require('./dboper');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '登录' });
+router.get('/', async function(req, res, next) {
+
+	let pageVars = { title: '登录' } ;
+
+	let roomList = await dboper.sql_select("select * from room where status<>3 order by id desc limit 20");
+
+	if (roomList) {
+
+		for(let i in roomList){
+			let roomUsers = await dboper.sql_select("select * from roomuser where roomid="+roomList[i].id);
+			if(roomUsers){
+				roomList[i].roomUsers = roomUsers ;
+			}else{
+				roomList[i].roomUsers = [] ;
+			}
+		}
+		pageVars.roomList = roomList ;
+	}else{
+		pageVars.roomList = [] ;
+	}
+
+  	res.render('index', pageVars);
 });
 
 module.exports = router;
